9df519bf50723fce188c32053619002485460066
Laura Derowitsch <derowitschlaura@gmail.com>
2020-02-19 15:11:17 -0600

    file distance

--- FileDistance/js/index.js
+++ FileDistance/js/index.js
@@ -6,12 +6,12 @@
  * then forces it into lowercase and tokenizes it into an array of words.
  */
 var tokenize = (text) => {
-  	text = text.replace(/[.,;@"”’“?‘\/#!$%\^&\*;:{}=\-_`~()]/g,"")
-  	text = text.replace(/\s{2,}/g," ");
-  	text = text.replace(/[\n\t]/g," ");
-	text = text.toLowerCase();
-	text = text.split(' ');
-	return text;
+    text = text.replace(/[.,;@"”’“?‘\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    text = text.replace(/\s{2,}/g, " ");
+    text = text.replace(/[\n\t]/g, " ");
+    text = text.toLowerCase();
+    text = text.split(' ');
+    return text;
 }
 
 
@@ -20,10 +20,39 @@ var tokenize = (text) => {
  * the searchWords in the text. The searchWords can be found in the text in either
  * order
  */
-var minDistance = (text,searchWords) => {
-	words = tokenize(text);
-	// TODO
-	return undefined;
+var minDistance = (text, searchWords) => {
+    words = tokenize(text);
+
+    distances = [];
+    wordOneLocations = [];
+    wordTwoLocations = [];
+
+    let i = 0;
+    for (i = 0; i < words.length; i++) {
+        if (words[i] == searchWords[0]) {
+            wordOneLocations.push(i);
+        } else if (words[i] == searchWords[1]) {
+            wordTwoLocations.push(i);
+        }
+    }
+
+    let j = 0;
+    i = 0;
+    for (i = 0; i < wordOneLocations.length; i++) {
+        for (j = 0; j < wordTwoLocations.length; j++) {
+            let distance = Math.abs(wordOneLocations[i] - wordTwoLocations[j] - 1);
+            distances.push(distance);
+        }
+    }
+
+    let currentMin = words.length;
+    for (i = 0; i < distances.length; i++) {
+        if (distances[i] < currentMin) {
+            currentMin = distances[i];
+        }
+    }
+
+    return currentMin;
 }
 
 
@@ -31,32 +60,32 @@ var minDistance = (text,searchWords) => {
  * This function displays the results on the html page
  */
 var displayResults = (m) => {
-	t = "The shortest distance between the searchWords in the text is <b>"+m+"</b> words.";
-	$('#results').append(t);
+    t = "The shortest distance between the searchWords in the text is <b>" + m + "</b> words.";
+    $('#results').append(t);
 }
 
 
 /*
  * This function handles the control flow of the program
  */
-$(document).ready(function(){
-	// chose one of the text and searchWords pairs below.
+$(document).ready(function() {
+    // chose one of the text and searchWords pairs below.
 
-	text = `The itsy bitsy spider climbed up the waterspout. 
+    text = `The itsy bitsy spider climbed up the waterspout. 
 			Down came the rain and washed the spider out. 
 			Out came the sun and dried up all the rain 
 			and the itsy bitsy spider climbed up the spout again.`
-	searchWords = ['spider','rain']
+    searchWords = ['spider', 'rain']
 
-	//text = prideandprejudice;
-	//searchWords = ['visit','happiness'];
+    text = prideandprejudice;
+    searchWords = ['visit', 'happiness'];
 
-	//text = peterpan;
-	//searchWords = ['fairy','hook']
+    // text = peterpan;
+    // searchWords = ['fairy', 'hook']
 
-	//text = dracula;
-	//searchWords = ['drink','blood']
+    // text = dracula;
+    // searchWords = ['drink', 'blood']
 
-	m = minDistance(text,searchWords);
-	displayResults(m);
+    m = minDistance(text, searchWords);
+    displayResults(m);
 });
